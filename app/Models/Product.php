a4287e15c49829cfad67947131a837d70ecababc
Burhan Yuswantyo <55840266+burhanyuswantyo@users.noreply.github.com>
2024-11-14 23:13:21 +0700

    Refactor : Add slug generator

--- app/Models/Product.php
+++ app/Models/Product.php
@@ -5,14 +5,27 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\Sluggable\HasSlug;
+use Spatie\Sluggable\SlugOptions;
 
 class Product extends Model
 {
     /** @use HasFactory<\Database\Factories\ProductFactory> */
-    use HasFactory, HasUlids;
+    use HasFactory, HasUlids, HasSlug;
 
     protected $guarded = ['id'];
     // protected $fillabel = ['name', 'description', 'price', 'category_id'];
+
+    /**
+     * Get the options for generating the slug.
+     */
+    public function getSlugOptions(): SlugOptions
+    {
+        return SlugOptions::create()
+            ->generateSlugsFrom('name')
+            ->saveSlugsTo('slug');
+    }
+
 
     public function category()
     {
