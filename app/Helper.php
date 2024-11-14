cebe83622fc8ed850ffc4408b8cd9fbcb413b112
Burhan Yuswantyo <55840266+burhanyuswantyo@users.noreply.github.com>
2024-11-14 23:13:01 +0700

    Refactor : Change default image from placeholder to blank image

--- app/Helper.php
+++ app/Helper.php
@@ -1,5 +1,6 @@
 <?php
 
+use Carbon\Carbon;
 use Illuminate\Support\Facades\File;
 
 if (!function_exists('productImage')) {
@@ -10,7 +11,28 @@ if (!function_exists('productImage')) {
         if (File::exists('storage/' . $image)) {
             return asset('storage/' . $image);
         } else {
-            return 'https://placehold.co/600x400/EEE/31343C';
+            return asset('images/blank-image.jpg');
         }
+    }
+}
+
+if (!function_exists('rupiah')) {
+    function rupiah($value)
+    {
+        return $value ? 'Rp ' . number_format($value, 0, ',', '.') : 'Rp 0';
+    }
+}
+
+if (!function_exists('formatDate')) {
+    function formatDate($date, $format = 'd F Y')
+    {
+        return $date ? Carbon::parse($date)->translatedFormat($format) : null;
+    }
+}
+
+if (!function_exists('formatHumanDate')) {
+    function formatHumanDate($date)
+    {
+        return $date ? Carbon::parse($date)->diffForHumans() : null;
     }
 }
